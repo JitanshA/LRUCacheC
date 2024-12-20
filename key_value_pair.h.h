9c049bdea24f0b1023a1469697fc0f699867f0fa
Jitansh Arora <arorajitansh32@gmail.com>
2024-12-20 19:10:36 -0400

    Added the header file for key_value pair, outlining the basic functionality

---
+++ key_value_pair.h.h
@@ -0,0 +1,31 @@
+#ifndef KEY_VALUE_PAIR_H
+#define KEY_VALUE_PAIR_H
+
+typedef struct kv_pair
+{
+    char *key;
+    char *value;
+} kv_pair_t;
+
+// Create a new key-value pair
+kv_pair_t *new_kv_pair(char *key, char *value);
+
+// Get the value associated with a key from a key-value pair
+char *get_value(kv_pair_t *kv_pair);
+
+// Update the value in a key-value pair
+void set_value(kv_pair_t *kv_pair, char *new_value);
+
+// Get the key from a key-value pair
+char *get_key(kv_pair_t *kv_pair);
+
+// Free a key-value pair
+void free_kv_pair(kv_pair_t *kv_pair);
+
+// Compare a key with the key in a key-value pair, return 1 if matches
+int matches_key(kv_pair_t *kv_pair, char *key);
+
+// Print a key-value pair
+void print_kv_pair(kv_pair_t *kv_pair);
+
+#endif
