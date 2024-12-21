0b1412c1e693486015f05161aeea86d6cf5d9608
Jitansh Arora <arorajitansh32@gmail.com>
2024-12-20 20:32:34 -0400

    Added functionality to get a value from cache, moving a node to front of table and temporary hash function

--- lru_cache.c
+++ lru_cache.c
@@ -41,8 +41,77 @@ LRUCache *lru_cache_create(int capacity) {
     return cache;
 }
 
-char *lru_cache_get(LRUCache *cache, char *key);
+char *lru_cache_get(LRUCache *cache, char *key) {
+    if (!cache || !key) {
+        return NULL;
+    }
+
+    int index = key_to_index(key, cache->capacity);
+
+    Node *node = cache->hash_table[index];
+
+    while(node) {
+        if (kv_pair_matches_key(key, node->kv_pair)) {
+            move_node_to_front(cache, node);
+            return kv_pair_get_value(node->kv_pair);
+        }
+
+        node = node->next;
+    }
+
+    return NULL;
+}
 
 void lru_cache_set(LRUCache *cache, char *key, char *value);
 
 void lru_cache_free(LRUCache *cache);
+
+// A simple mapping function to get the index of key in cache
+// Will replace later with a more complex function, commonly used in a cache
+int key_to_index(char *key, int capacity)
+{
+    if (!key || capacity <= 0) {
+        return -1;
+    }
+
+    int sum = 0;
+    while (*key)
+    {
+        sum += *key;
+        key++;
+    }
+
+    return sum % capacity;
+}
+
+void move_node_to_front(LRUCache *cache, Node *node) {
+    if (!cache || !node || cache->head == node) {
+        return;
+    }
+
+    if (node->next) {
+        node->next->prev = node->prev;
+    }
+    if (node->prev) {
+        node->prev->next = node->next;
+    }
+
+    if (cache->tail == node) {
+        cache->tail = node->prev;
+    }
+
+    node->next = cache->head;
+    node->prev = NULL;
+
+    if (cache->head)
+    {
+        cache->head->prev = node;
+    }
+
+    cache->head = node;
+
+    if (!cache->tail)
+    {
+        cache->tail = node;
+    }
+}
